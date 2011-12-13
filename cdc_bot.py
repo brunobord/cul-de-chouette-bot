cb677028efdaddf36c23435d0239f260561890bf
Bruno Bord <brunobord@gmail.com>
2011-12-13 23:33:00 +0100

    Cul De Chouette Bot, première version

---
+++ cdc_bot.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from cmdbot.core import Bot
+from cmdbot.decorators import admin, direct
+from game import Game, Combinations
+
+
+def in_game(f):
+    "Decorator: only process if the game is initialised"
+    def newfunc(bot, line):
+        if bot.game:
+            return f(bot, line)
+    return newfunc
+
+
+def in_running_game(f):
+    "Decorator: only process if the game is running"
+    def newfunc(bot, line):
+        if bot.game and bot.game.started:
+            return f(bot, line)
+    return newfunc
+
+
+def not_in_special_rule(f):
+    "Decorator: do not process if the game is in a special rule case"
+    def newfunc(bot, line):
+        if not bot.game.in_special_rule:
+            return f(bot, line)
+    return newfunc
+
+
+class CulDeChouetteBot(Bot):
+
+    welcome_message = "Salut la compagnie ! Ça vous dirait un cul-de-chouette ?"
+    exit_message = "Allez, ciao les enfants. Soyez sages."
+
+    @property
+    def game(self):
+        if not hasattr(self, '_game'):
+            self._game = Game()
+        return self._game
+
+    @direct
+    @admin
+    def do_init(self, line):
+        "Initialise a game"
+        logging.info('ici')
+        if hasattr(self, 'game') and self.game.started:
+            self.say("Non, on ne peut pas.")
+            return
+        self.say('Le jeu est prêt à commencer')
+        self.say('Qui veut jouer ? (dire "moi")')
+
+    @direct
+    @admin
+    @in_game
+    def do_start(self, line):
+        "Start the game"
+        if len(self.game.gamers) < 3:
+            self.say('Pas assez de joueurs. Inscrivez-vous en disant "moi"')
+            return
+        self.game.start()
+        self.say('Ça démarre... qui commence ?...')
+        self.say("c'est au tour de '%s'" % self.game.current_gamer)
+
+    @in_game
+    def do_scores(self, line):
+        "Say the gamer list, with scores"
+        s = []
+        for nick, score in self.game.gamers.items():
+            s.append('%s (%d)' % (nick, score))
+        self.say('Inscrits: %s' % ', '.join(s))
+
+    @in_game
+    def do_moi(self, line):
+        "Register the user"
+        if line.nick_from not in self.game.gamers:
+            self.game.gamers[line.nick_from] = 0
+            self.say('%s est inscrit' % line.nick_from)
+        else:
+            self.say('déjà inscrit...')
+
+    @in_game
+    @in_running_game
+    @not_in_special_rule
+    def do_roll(self, line):
+        "Roll the dice if it's your turn"
+        if line.nick_from == self.game.current_gamer:
+            dices = self.game.dices()
+            self.say('Les dés: %s' % ', '.join(map(str, dices)))
+            c = Combinations(dices)
+            score, messages, is_suite = c.resultat()
+            for message in messages:
+                self.say(message)
+            if is_suite:
+                self.game.in_suite = True
+                self.game.grelotte = []
+                return  # nothing else to do ATM
+            if score > 0:
+                self.say('%s gagne %d points' % (line.nick_from, score))
+                self.game.gamers[line.nick_from] += score
+
+            if self.game.gamers[line.nick_from] >= 343:
+                self.say("Le jeu est terminé ! C'est *%s* qui a gagné !" % str(line.nick_from))
+                self.game.stop()
+                return
+
+            self.game.next()
+            self.say("c'est au tour de '%s'" % self.game.current_gamer)
+        else:
+            self.say("C'est pas ton tour, manant !")
+
+    def do_grelotte(self, line):
+        "Grelotte ca picote"
+        if self.game.in_suite:
+            grelotte_line = ' '.join(line.message.split())
+            if grelotte_line.startswith(u'grelotte ça picote') \
+                    or grelotte_line.startswith(u'grelotte ca picote'):
+                if line.nick_from not in self.game.grelotte:
+                    self.game.grelotte.append(line.nick_from)
+            # check if everyone's here
+            difference = list(set(self.game.gamers.keys()).difference(set(self.game.grelotte)))
+            if len(difference) == 1:
+                gamer_nick = difference[0]
+                self.say('%s perd 10 points' % (gamer_nick))
+                self.game.gamers[gamer_nick] -= 10
+                self.game.next()
+                self.say("c'est au tour de '%s'" % self.game.current_gamer)
+
+        else:
+            # TODO: handle bevue
+            self.say('Bévue !')
+
+    @direct
+    @admin
+    def do_stop(self, line):
+        "Stop the current game"
+        self.say('Le jeu est arrêté')
+        self.brain['game'].stop()
+
+
+if __name__ == '__main__':
+    bot = CulDeChouetteBot()
+    bot.run()
---
+++ game.py
@@ -0,0 +1,125 @@
+#-*- coding: utf8 -*-
+"""Game module. Handles the rules, etc
+"""
+import random
+
+
+class Combinations(object):
+
+    def __init__(self, dices):
+        self.dices = dices
+
+    @property
+    def chouette(self):
+        temp = {}
+        for d in self.dices:
+            if d not in temp:
+                temp[d] = 0
+            temp[d] += 1
+        for key, value in temp.items():
+            if value == 2:
+                return key ** 2
+        return 1000
+
+    @property
+    def velute(self):
+        if self.dices[0] == (self.dices[1] + self.dices[2]):
+            return (self.dices[0] ** 2) * 2
+        if self.dices[1] == (self.dices[0] + self.dices[2]):
+            return (self.dices[1] ** 2) * 2
+        if self.dices[2] == (self.dices[0] + self.dices[1]):
+            return (self.dices[2] ** 2) * 2
+        return 0
+
+    @property
+    def chouette_velute(self):
+        temp = sorted(self.dices)
+        if temp in ([1, 1, 2], [2, 2, 4], [3, 3, 6],):
+            return (temp[-1] ** 2) * 2
+        return 0
+
+    @property
+    def cul_de_chouette(self):
+        temp = set(self.dices)
+        if len(temp) == 1:
+            return 40 + 10 * list(temp)[0]
+        return 0
+
+    @property
+    def suite(self):
+        temp = sorted(self.dices)
+        if temp in ([1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]):
+            return True
+        return False
+
+    def resultat(self):
+        lines = []
+        score = 0
+        is_suite = False
+        if self.chouette_velute:
+            lines.append("Chouette Velute: %d" % self.chouette_velute)
+            score = self.chouette_velute
+        elif self.chouette:
+            lines.append("Chouette: %d" % self.chouette)
+            score = self.chouette
+        elif self.velute:
+            lines.append("Velute: %d" % self.velute)
+            score = self.velute
+        if self.cul_de_chouette:
+            lines.append("Cul de Chouette: %d" % self.cul_de_chouette)
+            score = self.cul_de_chouette
+        if self.suite:
+            is_suite = True
+            lines.append("Suite ! Tout le monde doit dire")
+            lines.append("     'Grelotte ça picote'")
+            lines.append("Le dernier joueur perd 10 points")
+            # lines.append(u"En cas d'égalité, les joueurs se départagent en criant")
+            # lines.append(u"     Sans fin est la moisissure des bières bretonnes")
+            # lines.append(u"Toujours égalité ? jeter les dés, le moins fort perd 20 points.")
+            # lines.append(u"Si toujours égalité, on recommence pour perdre 30 points, 40 points, etc.")
+        if not lines:
+            lines.append('Néant... tu gagnes une grelottine')
+        return score, lines, is_suite
+
+
+class Game(object):
+
+    def __init__(self):
+        self.gamers = {}
+        self.started = False
+        self.opened_registration = True
+        self.in_suite = False
+        self.grelotte = []
+
+    def stop(self):
+        "Stop game"
+        self.started = False
+
+    def start(self):
+        "Start game"
+        self.started = True
+        self.turns = self.gamers.keys()
+        random.shuffle(self.turns)
+        self.current = 0
+
+    def next(self):
+        "Move turn to the next gamer"
+        self.current += 1
+        if self.current >= len(self.turns):
+            self.current = 0
+        # get outside any special rules
+        self.in_suite = False
+
+    @property
+    def current_gamer(self):
+        "Return current gamer"
+        return self.turns[self.current]
+
+    def dices(self):
+        "Return a triple dice roll"
+        return (random.randint(1, 6), random.randint(1, 6), random.randint(1, 6))
+
+    @property
+    def in_special_rule(self):
+        "Return 'True' if we're in a special rule process"
+        return self.in_suite
